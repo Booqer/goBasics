2841e5dfeac682c7dbb0da3b5764ec8b67aab273
Booqer <latrotoxx@gmail.com>
2018-10-09 02:54:34 -0400

    working on concurrency challenges

---
+++ 22_go-routines/19_channels_fan-out_fan-in/10_channelOfChannels/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+	"sync"
+	"time"
+)
+
+// the function to be run inside a goroutine. It recieves a channel on ch, sleeps for t, then sends t
+// on the channel it recieved
+func doStuff(t time.Duration, ch <-chan chan time.Duration) {
+	ac := <-ch
+	time.Sleep(t)
+	ac <- t
+}
+
+func main() {
+	// create the channel-over-channel type
+	sendCh := make(chan chan time.Duration)
+
+	recvCh := make(chan time.Duration)
+
+	// use this to block until all goroutines have reveived the ack and logged
+	var wg sync.WaitGroup
+
+	// start up 10 doStuff goroutines
+	for i := 0; i < 10; i++ {
+		go doStuff(time.Duration(i+1)*time.Second, sendCh)
+		// send channels to each doStuff goroutine. doStuff will "ack" by sending its sleep time back
+		sendCh <- recvCh
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			log.Printf("slept for %s", <-recvCh)
+		}()
+	}
+
+	wg.Wait()
+}
+
+// From https://www.goin5minutes.com/blog/channel_over_channel/
